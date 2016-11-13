43bccd9d52a6d6b5c34a4252ffec9e57f65cf6b6
Alpy <alperen.aydin@cpe.fr>
2016-11-12 20:52:12 -0500

    Initial commit for the project

---
+++ src/audio_dataset.py
@@ -0,0 +1,108 @@
+import scikits.audiolab as au
+import numpy as np
+
+import os
+
+# This is where our data is (I might change this to a more global variable in the 
+data_location = '../data/genres'
+genres= os.listdir(data_location)
+genres.sort() # I am sorting to make sure that the order stays the same regardless of the system
+
+# We are storing the filenames
+# But I still want to return them as np arrays so I will filter the output through this function
+def load_batch(list_fn):
+    data_point, fs, enc = au.auread(list_fn[0])
+
+    data = np.zeros((len(list_fn),data_point.shape[0],1,1), np.float32)
+
+    for i in range(list_fn.shape[0]):
+        data_point, fs, enc = au.auread(list_fn[i])
+        data[i,:,0,0] = data_point
+
+    return data
+
+class audio_dataset:
+    def __init__(self):
+        # We cut the data into two
+        train_data = []
+        train_label = []
+
+        valid_data = []
+        valid_label = []
+        # Since we don't have the space, we are only storing the 
+        for g in genres:
+            dirname = os.path.join(data_location,g)
+            filenames = os.listdir(dirname)
+            filenames.sort()
+            filenames = [os.path.join(dirname,fn) for fn in filenames]
+            for l in range(len(filenames)):
+                if l < 0.8*len(filenames):
+                    train_data.append(filenames[l])
+                    train_label.append([np.float32(g == genre) for genre in genres])
+                else:
+                    valid_data.append(filenames[l])
+                    valid_label.append([np.float32(g == genre) for genre in genres])
+
+        
+        train_data = np.array(train_data)
+        valid_data = np.array(valid_data)
+
+        train_label = np.array(train_label)
+        valid_label = np.array(valid_label)
+    
+        self.no_train = train_data.shape[0]
+        self.no_valid = valid_data.shape[0]
+        self.no_classes = valid_label.shape[1]
+        self.train_data = train_data
+        self.train_label = train_label
+        self.valid_data = valid_data
+        self.valid_label = valid_label
+
+        self.index_in_epoch = 0
+        self.index_in_valid = 0
+        self.completed_epochs = 0
+
+        self.get_new_permutation()
+
+    # We assume that the data will be fed randomly
+    # We randomize the output of the train data
+    def get_new_permutation(self):
+        self.perm = np.arange(self.no_train)
+        np.random.shuffle(self.perm)
+
+    # Returns the next batch of the TRAINING SET
+    # The set is shuffled after each epoch
+    def next_batch_train(self,batch_size):
+        start = self.index_in_epoch
+        self.index_in_epoch += batch_size
+        if self.index_in_epoch > self.no_train:
+            self.completed_epochs += 1
+
+            #Reshuffle the data
+            self.get_new_permutation()
+            start = 0
+            self.index_in_epoch = batch_size
+
+        end = self.index_in_epoch
+        cur_perm = self.perm[start:end]
+        return load_batch(self.train_data[cur_perm]), self.train_label[cur_perm], batch_size
+
+    def reset_batch_valid(self):
+        self.index_in_valid = 0
+
+    # This returns the next batch of the VALIDATION TEST
+    # No shuffling
+    def next_batch_valid(self, batch_size):
+        start = self.index_in_valid
+        self.index_in_valid +=batch_size
+        if self.index_in_valid > self.no_valid:
+            self.reset_batch_valid()
+            return -1, -1, -1
+        end = self.index_in_valid
+        return load_batch(self.valid_data[start:end]), self.valid_label[start:end], batch_size
+
+
+
+
+
+        
---
+++ src/train.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import cnn_functions as cf
+from audio_dataset import audio_dataset
+
+
+dataset = audio_dataset()
+wave,l,bs = dataset.next_batch_valid(10)
+print wave.shape
+
+#Parametres of the loop
+LOG_STEP = 200
+SAVER_STEP = 100
+
+# Hyper-parameters of the network
+BATCH_SIZE = 10
+
+x = tf.placeholder(tf.float32, [None, 661794,1,1])
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+# We are going to do a series of convolution+MaxPooling to reduce the size of the sound wave
+
+h1 = cf.cnm2x1Layer(x, [7,1,1,3]) # size=330897x3
+h2 = cf.cnm2x1Layer(h1, [7,1,3,3]) # size=165448x3
+
+h3 = cf.cnm2x1Layer(h2, [5,1,3,5]) # size=82724x5
+h4 = cf.cnm2x1Layer(h3, [5,1,5,5]) # size=41362x5
+h5 = cf.cnm2x1Layer(h4, [3,1,5,5]) # size=20681x5
+h6 = cf.cnm2x1Layer(h5, [3,1,5,5]) # size=10340x5
+h7 = cf.cnm2x1Layer(h6, [8,1,5,5]) # size=5170x5
+
+hf = tf.reshape(h7, [-1, 5170*5])
+
+fc1 = cf.fc_nn(hf,[5170*5,100])
+fc2 = cf.fc_nn(fc1,[100,10])
+
+
+y = tf.nn.softmax(fc2)
+
+loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# We are using the Adam Optimiser (because it is very good at managing the learning rate and momentum. Plus it is very easy to use and I am very llazy)
+train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+with sess.as_default():
+    for s in range(1+int(2e6)):
+        waves, labels, bs = dataset.next_batch_train(BATCH_SIZE)
+
+
+
+        train_step.run(feed_dict={x:waves, y_:labels})
