3ee747f54abaff1401bebc59d74f59698560a80e
Alpy <alperen.aydin@cpe.fr>
2017-01-28 20:02:05 -0500

    Modified the dataset file

--- src/audio_dataset.py
+++ src/audio_dataset.py
@@ -2,29 +2,23 @@ import scikits.audiolab as au
 import numpy as np
 
 import os
+import math
 
 # IMPORTANT NOTE: This code is designed to work on my 4GB laptop
-# When I figure out how to run TF on IBM's BLUEMIX I will have code that loads everything into memory
+# When I figure out how to run TF on IBM's BLUEMIX I will have code that
+# loads everything into memory
 
-# This is where our data is (I might change this to a more global variable in the 
+# This is where our data is (I might change this to a more global variable
+# in the
 data_location = '../data/genres'
-genres= os.listdir(data_location)
-genres.sort() # I am sorting to make sure that the order stays the same regardless of the system
+genres = os.listdir(data_location)
+genres.sort()
+# I am sorting to make sure that the order stays the same regardless of
+# the system
 
-# We are storing the filenames
-# But I still want to return them as np arrays so I will filter the output through this function
-def load_batch(list_fn):
-    data_point, fs, enc = au.auread(list_fn[0])
-
-    data = np.zeros((len(list_fn),524288,1,1), np.float32)
-
-    for i in range(list_fn.shape[0]):
-        data_point, fs, enc = au.auread(list_fn[i])
-        data[i,:,0,0] = data_point[0:524288]
-
-    return data
 
 class audio_dataset:
+
     def __init__(self):
         # We cut the data into two parts
         # Since we do not have enough information we will do 80% train and 20%
@@ -33,30 +27,36 @@ class audio_dataset:
 
         valid_data = []
         valid_label = []
-        # Since we don't have the space, we are only loading the filenames in memory
-        # We will use load_batch() above to load the audio files when we need them.
-        
+        # Since we don't have the space,
+        # we are only loading the filenames in memory
+        # We will use load_batch() above to load the audio files when we need
+        # them.
+        self.max_length = 0
         for g in genres:
-            dirname = os.path.join(data_location,g)
+            dirname = os.path.join(data_location, g)
             filenames = os.listdir(dirname)
             filenames.sort()
-            filenames = [os.path.join(dirname,fn) for fn in filenames]
+            filenames = [os.path.join(dirname, fn) for fn in filenames]
             for l in range(len(filenames)):
-                if l < 0.8*len(filenames):
+                file, _, _ = au.auread(filenames[l])
+                if(self.max_length < file.shape[0]):
+                    self.max_length = file.shape[0]
+                if l < 0.8 * len(filenames):
                     train_data.append(filenames[l])
-                    train_label.append([np.float32(g == genre) for genre in genres])
+                    train_label.append([np.float32(g == genre)
+                                        for genre in genres])
                 else:
                     valid_data.append(filenames[l])
-                    valid_label.append([np.float32(g == genre) for genre in genres])
-
-        
+                    valid_label.append([np.float32(g == genre)
+                                        for genre in genres])
+        self.max_length = 2**(int(math.log(self.max_length, 2)))
         train_data = np.array(train_data)
         valid_data = np.array(valid_data)
 
         train_label = np.array(train_label)
         valid_label = np.array(valid_label)
 
-        # Setting a bunch of variables 
+        # Setting a bunch of variables
         self.no_train = train_data.shape[0]
         self.no_valid = valid_data.shape[0]
         self.no_classes = valid_label.shape[1]
@@ -77,39 +77,44 @@ class audio_dataset:
         self.perm = np.arange(self.no_train)
         np.random.shuffle(self.perm)
 
+    # We are storing the filenames
+    # But I still want to return them as np arrays so I will filter the output
+    # through this function
+    def load_batch(self, filename):
+        data = np.zeros((1, self.max_length, 1, 1), np.float32)
+        data_point, fs, enc = au.auread(filename)
+        data[0, 0:self.max_length, 0, 0] = data_point[0:self.max_length]
+        return data
+
     # Returns the next batch of the TRAINING SET
     # The set is shuffled after each epoch
-    def next_batch_train(self,batch_size):
+    def next_batch_train(self):
         start = self.index_in_epoch
-        self.index_in_epoch += batch_size
+        self.index_in_epoch += 1
         if self.index_in_epoch > self.no_train:
             self.completed_epochs += 1
 
-            #Reshuffle the data
+            # Reshuffle the data
             self.get_new_permutation()
             start = 0
-            self.index_in_epoch = batch_size
+            self.index_in_epoch = 0
 
-        end = self.index_in_epoch
-        cur_perm = self.perm[start:end]
-        return load_batch(self.train_data[cur_perm]), self.train_label[cur_perm], batch_size
+        cur_perm = self.perm[start]
+        return (self.load_batch(self.train_data[cur_perm]),
+                self.train_label[cur_perm],
+                1)
 
     def reset_batch_valid(self):
         self.index_in_valid = 0
 
     # This returns the next batch of the VALIDATION TEST
     # No shuffling
-    def next_batch_valid(self, batch_size):
+    def next_batch_valid(self):
         start = self.index_in_valid
-        self.index_in_valid +=batch_size
+        self.index_in_valid += 1
         if self.index_in_valid > self.no_valid:
             self.reset_batch_valid()
             return -1, -1, -1
-        end = self.index_in_valid
-        return load_batch(self.valid_data[start:end]), self.valid_label[start:end], batch_size
-
-
-
-
-
-        
+        return (self.load_batch(self.valid_data[start]),
+                self.valid_label[start],
+                1)
--- src/rcnn_train.py
+++ src/rcnn_train.py
@@ -1,5 +1,5 @@
 # Author: Alperen AYDIN
-# I am going to try out a hybrid network that uses both convolutional and 
+# I am going to try out a hybrid network that uses both convolutional and
 
 import tensorflow as tf
 import cnn_functions as cf
@@ -8,42 +8,45 @@ from audio_dataset import audio_dataset
 
 dataset = audio_dataset()
 
-#Parameters of the loop
+# Parameters of the loop
 LOG_STEP = 10
 SAVER_STEP = 100
 
 # Hyper-parameters of the network
-BATCH_SIZE = 10
+BATCH_SIZE = 1
 
-# The inputs 
-x = tf.placeholder(tf.float32, [None, 524288,1,1])
+# The inputs
+x = tf.placeholder(tf.float32, [1, 600000, 1, 1])
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-# We are going to do a series of convolution+MaxPooling to reduce the size of the sound wave
+X = tf.reshape(x, [10, 60000, 1, 1])
+# We are going to do a series of convolution+MaxPooling to reduce the size
+# of the sound wave
 
-h1 = cf.cnm2x1Layer(x, [7,1,1,3]) # size=262144x3
-h2 = cf.cnm2x1Layer(h1, [7,1,3,3]) # size=131072x3
+h1 = cf.cnm2x1Layer(X, [7, 1, 1, 3])   # size=30000x3
+h2 = cf.cnm2x1Layer(h1, [7, 1, 3, 3])  # size=15000x3
 
-h3 = cf.cnm2x1Layer(h2, [5,1,3,5]) # size=65536x5
-h4 = cf.cnm2x1Layer(h3, [5,1,5,5]) # size=32768x5
-h5 = cf.cnm2x1Layer(h4, [3,1,5,1]) # size=16384x1
+h3 = cf.cnm2x1Layer(h2, [5, 1, 3, 5])  # size=7500x5
+h4 = cf.cnm2x1Layer(h3, [5, 1, 5, 5])  # size=3750x5
+h5 = cf.cnm2x1Layer(h4, [3, 1, 5, 1])  # size=1875x1
 
-t = tf.reshape(h5, [-1, 16384, 1])
+t = tf.reshape(h5, [-1, 1875, 1])
 
 # Defining the LSTM cell
 num_hidden = 15
-cell = tf.nn.rnn_cell.LSTMCell(num_hidden,state_is_tuple=True)
+cell = tf.nn.rnn_cell.LSTMCell(num_hidden, state_is_tuple=True)
 
 val, state = tf.nn.dynamic_rnn(cell, t, dtype=tf.float32)
 
-               
+val = tf.reshape(val, [1, 18750, 15])
+
 # Where are interested in the result we get in the end
 # This might be improved
 val = tf.transpose(val, [1, 0, 2])
 last = tf.gather(val, int(val.get_shape()[0]) - 1)
 
 # We want to map the 24 member output to 10-long vector
-fc1 = cf.fc_nn(last, [num_hidden,10])
+fc1 = cf.fc_nn(last, [num_hidden, 10])
 
 # We pass the output through softmax so it represents probabilities
 y = tf.nn.softmax(fc1)
@@ -52,34 +55,35 @@ y = tf.nn.softmax(fc1)
 # We chose this as it offers better results for classification
 loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-# We are using the Adam Optimiser because it is effective at managing the learning rate and momentum
+# We are using the Adam Optimiser because it is effective at managing the
+# learning rate and momentum
 train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
 # Classification accuracy is a better indicator of performance
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
 
-               
-log = open('logs/rcnn_log.txt','a')
 
-saver      = tf.train.Saver()
+log = open('logs/rcnn_log.txt', 'a')
+
+saver = tf.train.Saver()
 checkpoint = 0
 
 with sess.as_default():
-    for s in range(1,int(2e6)):
+    for s in range(1, int(2e6)):
         waves, labels, bs = dataset.next_batch_train(BATCH_SIZE)
         print 'step {}'.format(s)
 
         # We update the log with the newest performance results
-        if (s%LOG_STEP==0):
+        if (s % LOG_STEP == 0):
             # We calculate the performance results
             # for the training set on the current batch
-            tr_y = y.eval(feed_dict={x:waves})
-            train_loss = loss.eval(feed_dict={y:tr_y, y_:labels})
-            train_acc = accuracy.eval(feed_dict={y:tr_y, y_:labels})
+            tr_y = y.eval(feed_dict={x: waves})
+            train_loss = loss.eval(feed_dict={y: tr_y, y_: labels})
+            train_acc = accuracy.eval(feed_dict={y: tr_y, y_: labels})
 
             # For the validation set, we do it on the whole thing
             # The final results are means of the results for each batch
@@ -90,23 +94,25 @@ with sess.as_default():
                 va_x, va_y_, bs = dataset.next_batch_valid(BATCH_SIZE)
                 if bs == -1:
                     break
-                batch_count +=1.0
-                
-                va_y = y.eval(feed_dict={x:va_x})
-                valid_loss += loss.eval(feed_dict={y:va_y, y_:va_y_})
-                valid_acc += accuracy.eval(feed_dict={y:va_y, y_:va_y_})
-                
-            valid_loss = valid_loss/batch_count
-            valid_acc = (valid_acc)/batch_count
-            #Adding a new line to the log
+                batch_count += 1.0
+
+                va_y = y.eval(feed_dict={x: va_x})
+                valid_loss += loss.eval(feed_dict={y: va_y, y_: va_y_})
+                valid_acc += accuracy.eval(feed_dict={y: va_y, y_: va_y_})
+
+            valid_loss = valid_loss / batch_count
+            valid_acc = (valid_acc) / batch_count
+            # Adding a new line to the log
             logline = 'Epoch {} Batch {} train_loss {} train_acc {} valid_loss {} valid_acc {} \n'
-            logline = logline.format(dataset.completed_epochs, s, train_loss, train_acc, valid_loss, valid_acc)
+            logline = logline.format(
+                dataset.completed_epochs, s, train_loss, train_acc, valid_loss, valid_acc)
             log.write(logline)
             print logline
 
-        if s%SAVER_STEP==0:
-            path = saver.save(sess, 'checkpoints/rcnn_',global_step=checkpoint)
+        if s % SAVER_STEP == 0:
+            path = saver.save(sess, 'checkpoints/rcnn_',
+                              global_step=checkpoint)
             print "Saved checkpoint to %s" % path
             checkpoint += 1
-            
-        train_step.run(feed_dict={x:waves, y_:labels})
+
+        train_step.run(feed_dict={x: waves, y_: labels})
